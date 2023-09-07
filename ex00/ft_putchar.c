bd043041e939d18ec75e9e433e430910c9f77988
Malalatiana Rabarison <marabari@student.42.fr>
2023-09-07 12:42:01 +0200

    c00

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: marabari <marabari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 11:31:42 by marabari          #+#    #+#             */
-/*   Updated: 2023/09/06 22:53:54 by marabari         ###   ########.fr       */
+/*   Updated: 2023/09/07 12:41:08 by marabari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ void	ft_putchar(char c)
 
 /*int	main(void)
 {
-	ft_putchar('c');
+	char	test;
+
+	test = 'd';
+	ft_putchar(test);
 	return (0);
 }*/
